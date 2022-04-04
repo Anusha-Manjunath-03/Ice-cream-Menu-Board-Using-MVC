717dbdc9158890f13c437e31fd6d99b9257cc7c7
Anusha M <56243327+Anusha-Manjunath-03@users.noreply.github.com>
2022-04-04 10:53:34 +0530

    Delete insert.sql

--- insert.sql
+++
@@ -1,8 +0,0 @@
-\c menu
-
-INSERT into Items values(1,'Mango',60.0);
-INSERT into Items values(2,'Chocolate',90.0);
-INSERT into Items values(3,'Vanilla',30.0);
-INSERT into Items values(4,'Guava',120.0);
-INSERT into Items values(5,'Strawberry',70.0);
-INSERT into Items values(6,'Blueberry',120.0);
